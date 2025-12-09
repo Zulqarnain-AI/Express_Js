407dd9d6b3234a71b30c38d78fa77899c0c93638
Zulqarnain-AIgit config --global user.name Zulqarnain-AIgit config --global user.name Zulqarnain-AI <qarnainhassan786@gmail.com>
2025-12-09 16:48:22 +0500

    day 4 of express js

---
+++ day_4/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+
+const dbName = 'College';
+const url = 'mongodb://localhost:27017/';
+const client = new MongoClient(url)
+
+const app = express();
+app.set('view engine','ejs');
+app.get('/',async (req,res)=>{
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection('student')
+    const result = await collection.find().toArray()
+    res.render('home',{result});
+})
+app.listen(3000)
+

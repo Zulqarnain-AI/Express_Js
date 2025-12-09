c8bbba959ee846c1c5002c8617909e6aaba76522
Zulqarnain-AIgit config --global user.name Zulqarnain-AIgit config --global user.name Zulqarnain-AI <qarnainhassan786@gmail.com>
2025-12-09 13:08:31 +0500

    day 3 of express js

---
+++ day_3/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path'
+
+const app = express();
+    const abspath = path.resolve('pages')
+
+    // middleware
+function checkRoute(req,res,next){
+    console.log(req.url);
+    next();
+
+}
+app.use(checkRoute)//middleware called
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(abspath+"/home.html");
+})
+app.get('/about',(req,res)=>{
+    res.sendFile(abspath+"/about.html");
+})
+app.use((req,res)=>{
+    res.status(404).sendFile(abspath+"/pageNotFound.html")
+})
+
+app.listen(3000);

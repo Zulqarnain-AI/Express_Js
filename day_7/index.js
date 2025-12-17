c27e34d6080c905df8d44176bf5f573382418a85
Zulqarnain-AIgit config --global user.name Zulqarnain-AIgit config --global user.name Zulqarnain-AI <qarnainhassan786@gmail.com>
2025-12-17 23:41:30 +0500

    day 7 of express js

--- day_7/index.js
+++ day_7/index.js
@@ -1,17 +1,32 @@
-import express from 'express'
-import {MongoClient} from 'mongodb'
-const dbname= "College";
+import express,{urlencoded} from 'express'
+import { MongoClient } from 'mongodb'
+
+const dbname = "College";
 const url = "mongodb://localhost:27017/";
 const client = new MongoClient(url)
 const app = express();
-app.set('view engine','ejs')
-client.connect().then(res=>{
-    const db=res.db(dbname);
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:true}))
+client.connect().then(res => {
+    const db = res.db(dbname);
     const collection = db.collection("student");
 
-    app.get('/',async(req,res)=>{
+    app.get('/', async (req, res) => {
         const data = await collection.find().toArray();
-        res.render('home',{data})
+        res.render('home', { data })
+    })
+
+    app.get('/api', async (req, res) => {
+        const response = await collection.find().toArray()
+        res.send(response)
+    })
+    app.get('/addStudent', (req, res) => {
+        res.render('addStudent')
+    })
+    app.post('/insertStudent',(req,res)=>{
+        collection.insertOne(req.body)
+        res.send(`added successfully`)
+
     })
 })
 
